171f0fa4b6fc23ab955176d27c4dd14568ed4849
Maxime Florile <florile.maxime@gmail.com>
2016-04-06 15:08:06 +0200

    commit 1

---
+++ BanditManchot/PlayViewController.swift
@@ -0,0 +1,64 @@
+//
+//  PlayViewController.swift
+//  BanditManchot
+//
+//  Created by Maxime Florile on 06/04/2016.
+//  Copyright © 2016 Maxime Florile. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [[String]] = [[String]]()
+    
+
+    @IBAction func test(sender: UISwipeGestureRecognizer) {
+        print("salut")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Input data into the Array:
+        pickerData = [["1", "2", "3", "4"],
+            ["a", "b", "c", "d"],
+            ["!", "#", "$", "%"],
+            ["w", "x", "y", "z"]]
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
