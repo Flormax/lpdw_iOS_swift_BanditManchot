08924759dec3d685a9dcd2f73c5b99e0f82ddecc
Maxime Florile <florile.maxime@gmail.com>
2016-04-07 16:19:16 +0200

    piker

--- BanditManchot/PlayViewController.swift
+++ BanditManchot/PlayViewController.swift
@@ -7,30 +7,58 @@
 //
 
 import UIKit
-import PickerView
 
-class PlayViewController: UIViewController, PickerViewDelegate, PickerViewDataSource {
+class PlayViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var imgBandit: UIImageView!
-    @IBOutlet weak var picker: PickerView!
     @IBOutlet weak var numberCoins: UILabel!
     @IBOutlet weak var buttonImage: UIImageView!
     var coins:Int = 10
     var timer = NSTimer()
     var pickerData: [String] = [String]()
+    var itemNameList = [String]()
+    var selectedItemName = String()
+    var itemPhotoList = [UIImage]()
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.numberCoins.text = String(coins)
         self.buttonImage.image = UIImage(named:"Image")
         self.imgBandit.image = UIImage(named:"Image-2")
-        pickerData = ["1", "2", "3", "4"]
+        
+        itemNameList = ["bar", "bar", "bar" , "bar" , "bar" , "bar",
+            "bar", "bar", "bar" , "bar" , "bar" , "bar",
+            "bar", "bar", "bar" , "bar" , "bar" , "bar"]
+        
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+        itemPhotoList.append(UIImage(named: "bar")!)
+
         
         picker.dataSource = self
         picker.delegate = self
-        picker.scrollingStyle = .Infinite
     }
     
     @IBAction func test(sender: UISwipeGestureRecognizer) {
@@ -51,47 +79,49 @@ class PlayViewController: UIViewController, PickerViewDelegate, PickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
     
-    func numberOfRowsInPickerView(pickerView: PickerView) -> Int {
-        return pickerData.count
-    }
-    
-    func pickerView(pickerView: PickerView, titleForRow row: Int) -> String {
-        return pickerData[row]
-    }
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        let view = UIView(frame: CGRectMake(0, 0, 30, 30))
         
-    func pickerView(pickerView: PickerView, didSelectRow row: Int) {
-        print("The selected item is \(pickerData[row])")
+        let imageView = UIImageView(frame: CGRectMake(0, 0, 30, 30))
+        imageView.image = itemPhotoList[row]
+        
+        let labelView = UILabel(frame: CGRectMake(0, 0, 0, 0))
+        labelView.text = itemNameList[row]
+        
+        view.addSubview(imageView)
+        view.addSubview(labelView)
+        
+        return view
     }
     
-    func heightForRowInPickerView(pickerView: PickerView) -> CGFloat {
-        return 50.0 // In this example I'm returning arbitrary 50.0pt but you can return the row height you want.
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
     }
     
-    func styleForLabel(label: UILabel, inPickerView pickerView: PickerView) {
-        label.textAlignment = .Center
-        label.font = UIFont.systemFontOfSize(15.0)
-        label.textColor = .lightGrayColor()
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return itemNameList.count
     }
     
-    func styleForHighlightedLabel(label: UILabel, inPickerView pickerView: PickerView) {
-        label.textAlignment = .Center
-        label.font = UIFont.systemFontOfSize(25.0) // Change the size of font
-        label.textColor = view.tintColor // Change the text color
+    
+    // MARK: UIPickerViewDelegate functions
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
     }
     
-
-    
-    
-    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
     
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
